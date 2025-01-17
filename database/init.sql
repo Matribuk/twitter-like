e5f0889d34f17e4f284c6add18af7973d73eb50e
Matribuk <antonin.leprest@epitech.eu>
2025-01-17 17:03:06 +0100

    [ADD] Database
    Signed-off-by: Matribuk <antonin.leprest@epitech.eu>

---
+++ database/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE socialapp;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    nickname VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (email, nickname, password)
+VALUES ('admin@example.com', 'administrator', 'admin123');
